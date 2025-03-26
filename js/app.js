9369b89d4cf5028800033d05b36bb629fc3969f9
SergioCaMi <sergiocami84@gmail.com>
2025-03-26 19:42:18 +0100

    fin ejercicio

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-// Tu código aquí.
+// Estados de la app
+// Estado inicial
+const btnLower = document.querySelector(".prevBtn");
+const btnNext = document.querySelector(".nextBtn");
+const displayCounter = document.querySelector("#counter");
+var counter = 0;
+
+
+// Asociamos eventos
+
+btnLower.addEventListener("click", ()=>{
+    //Actualizamos variables
+    counter--;
+    //Actualizamos DOM
+    displayCounter.textContent = counter;
+    if (counter<0)displayCounter.style.color = "red";
+    if (counter==0)displayCounter.style.color = "black";
+});
+
+btnNext.addEventListener("click", ()=>{
+    //Actualizamos variables
+    counter++;
+    //Actualizamos DOM
+    displayCounter.textContent = counter;
+    if (counter>0)displayCounter.style.color = "green";
+    if (counter==0)displayCounter.style.color = "black";
+
+});
+
+
+
